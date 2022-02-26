338dbe2e810bfe4219f851ba9a1a523e884572cd
vyankagrawal <vyankagrawal@gmail.com>
2022-02-26 18:22:37 +0530

    Day2JavaProjHW2

---
+++ Day2JavaProjHW2/src/mypackage/MyClass.java
@@ -0,0 +1,18 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				// TODO Auto-generated method stub
+				int i;
+				for(i=1;i<=5;i++)
+				{
+				System.out.println(i);
+			
+			}
+
+	}
+
+}
